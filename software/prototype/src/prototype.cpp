292ab165db716a183227434324805c02ce2caa20
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2023-03-20 20:26:39 +0200

    Complete chip emulation in prototype

--- software/prototype/src/prototype.cpp
+++ software/prototype/src/prototype.cpp
@@ -1,220 +1,372 @@
-#if 0
-
-/*
-This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
-or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-*/
-
-#include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <malloc.h>
 #include <Windows.h>
 #include <MMSystem.h>
 
-static inline uint8_t THREEQUARTERS(uint8_t x)
+#undef min
+#undef max 
+
+#include <fstream>
+#include <algorithm>
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const uint32_t F_CPU        = 8000000;
+const uint32_t F_PSG        = 1750000;
+const uint8_t  PWM_BITS     = 8;
+const uint8_t  PWM_CHANNELS = 1;
+
+const uint8_t  FRAME_RATE   = 50;
+const uint32_t SAMPLE_RATE  = (F_CPU / (1 << PWM_BITS));//(F_CPU / 289);
+const uint8_t  SAMPLE_SIZE  = (PWM_BITS / 8 * PWM_CHANNELS);
+const uint16_t PSG_C_PER_S  = (F_PSG / 8 / SAMPLE_RATE);
+const uint16_t BUFFER_SIZE  = (SAMPLE_RATE / FRAME_RATE) * SAMPLE_SIZE;
+
+// PSG registers
+#pragma pack(push, 1)
+union {
+	struct {
+		uint16_t a_period;
+		uint16_t b_period;
+		uint16_t c_period;
+		uint8_t  n_period;
+		uint8_t  mixer;
+		uint8_t  a_volume;
+		uint8_t  b_volume;
+		uint8_t  c_volume;
+		uint16_t e_period;
+		uint8_t  e_shape;
+	};
+	uint8_t reg[14];
+} psg;
+
+struct {
+	uint16_t a_counter; // tone A generator period counter
+	uint16_t b_counter; // tone B generator period counter
+	uint16_t c_counter; // tone C generator period counter
+	uint8_t  n_counter; // noise generator period counter
+	uint16_t n_shift;   // noise generator shift register
+	uint16_t e_counter; // envelope generator period counter
+	uint8_t  e_config;
+	uint8_t  e_step;    // envelope generator shape counter 
+	uint8_t  flags;     // S.P.a.b.c.C.B.A
+	                    // | | | | | | | |
+	                    // | | *-*-* | | *-> tone A generator waveform
+	                    // | |   |   | *---> tone B generator waveform
+	                    // | |   |   *-----> tone C generator waveform
+	                    // | |   *---------> noise generator waveform
+						// | *-------------> noise generator prescale
+	                    // *---------------> envelope generator segment
+} state;
+
+#pragma pack(pop)
+
+const float max_amp = 170;
+
+const uint8_t amplitude4b[] =
 {
-	return (x >> 2) + (x >> 1);
+	uint8_t(0.0f + max_amp * 0.0056),
+	uint8_t(0.5f + max_amp * 0.0079),
+	uint8_t(0.5f + max_amp * 0.0112),
+	uint8_t(0.5f + max_amp * 0.0158),
+	uint8_t(0.5f + max_amp * 0.0224),
+	uint8_t(0.5f + max_amp * 0.0316),
+	uint8_t(0.5f + max_amp * 0.0447),
+	uint8_t(0.5f + max_amp * 0.0631),
+	uint8_t(0.5f + max_amp * 0.0891),
+	uint8_t(0.5f + max_amp * 0.1259),
+	uint8_t(0.5f + max_amp * 0.1778),
+	uint8_t(0.5f + max_amp * 0.2512),
+	uint8_t(0.5f + max_amp * 0.3548),
+	uint8_t(0.5f + max_amp * 0.5012),
+	uint8_t(0.5f + max_amp * 0.7079),
+	uint8_t(0.5f + max_amp * 1.0000)
+};
+
+const uint8_t amplitude5b[] =
+{
+	uint8_t(0.0f + max_amp * 0.0047),
+	uint8_t(0.5f + max_amp * 0.0056),
+	uint8_t(0.5f + max_amp * 0.0067),
+	uint8_t(0.5f + max_amp * 0.0079),
+	uint8_t(0.5f + max_amp * 0.0094),
+	uint8_t(0.5f + max_amp * 0.0112),
+	uint8_t(0.5f + max_amp * 0.0133),
+	uint8_t(0.5f + max_amp * 0.0158),
+	uint8_t(0.5f + max_amp * 0.0188),
+	uint8_t(0.5f + max_amp * 0.0224),
+	uint8_t(0.5f + max_amp * 0.0266),
+	uint8_t(0.5f + max_amp * 0.0316),
+	uint8_t(0.5f + max_amp * 0.0376),
+	uint8_t(0.5f + max_amp * 0.0447),
+	uint8_t(0.5f + max_amp * 0.0531),
+	uint8_t(0.5f + max_amp * 0.0631),
+	uint8_t(0.5f + max_amp * 0.0750),
+	uint8_t(0.5f + max_amp * 0.0891),
+	uint8_t(0.5f + max_amp * 0.1059),
+	uint8_t(0.5f + max_amp * 0.1259),
+	uint8_t(0.5f + max_amp * 0.1496),
+	uint8_t(0.5f + max_amp * 0.1778),
+	uint8_t(0.5f + max_amp * 0.2113),
+	uint8_t(0.5f + max_amp * 0.2512),
+	uint8_t(0.5f + max_amp * 0.2985),
+	uint8_t(0.5f + max_amp * 0.3548),
+	uint8_t(0.5f + max_amp * 0.4217),
+	uint8_t(0.5f + max_amp * 0.5012),
+	uint8_t(0.5f + max_amp * 0.5957),
+	uint8_t(0.5f + max_amp * 0.7079),
+	uint8_t(0.5f + max_amp * 0.8414),
+	uint8_t(0.5f + max_amp * 1.0000)
+};
+
+enum { INC = +1, DEC = -1, HLD = 0, TOP = 0x1F, BOT = 0x00 };
+const uint8_t envelopes[]
+{
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(DEC), uint8_t(TOP),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(INC), uint8_t(BOT),
+	uint8_t(DEC), uint8_t(TOP), uint8_t(HLD), uint8_t(TOP),
+	uint8_t(INC), uint8_t(BOT), uint8_t(INC), uint8_t(BOT),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(TOP),
+	uint8_t(INC), uint8_t(BOT), uint8_t(DEC), uint8_t(TOP),
+	uint8_t(INC), uint8_t(BOT), uint8_t(HLD), uint8_t(BOT)
+};
+
+void avr_init()
+{
+	memset(&psg, 0, sizeof(psg));
+	memset(&state, 0, sizeof(state));
+	state.flags = 0b11000111;
 }
 
-const uint16_t notes[] = {
-	-1, 134, 159, 179, 201, 213, 239, 268, 301, 319, 358, 401, 425, 451, 477, 536, 601, 637, 715
-};
-#define arpnotes (notes + 5)
-
-const uint16_t arpeggio[][2] = {
-	{ 0x24, 0x6A },
-	{ 0x46, 0x9C },
-	{ 0x13, 0x59 },
-	{ 0x02, 0x47 },
-	{ 0x24, 0x59 },
-	{ 0x24, 0x58 },
-	{ 0x57, 0xAD },
-	{ 0x35, 0x9B }
-};
-
-#define ARPSIZE 76
-
-#if 0
-uint8_t arpseq1[4][8] = {
-	{ 0, 0, 1, 2, 0, 0, 6, 2, },
-	{ 0, 0, 1, 2, 0, 0, 1, 7, },
-	{ 0, 0, 1, 2, 0, 0, 1, 2, },
-	{ 3, 3, 2, 2, 0, 0, 4, 5, },
-};
-#else
-const uint8_t arpseq1[4][4] = {
-	{ 0x00, 0x12, 0x00, 0x62 },
-	{ 0x00, 0x12, 0x00, 0x17 },
-	{ 0x00, 0x12, 0x00, 0x12 },
-	{ 0x33, 0x22, 0x00, 0x45 },
-};
-#endif
-const int arpseq2[] = { 0, 1, 0, 1, 0, 1, 0, 2, 3, 3 };
-
-//const uint32_t arptiming = B32(00001100,00110000,11111011,00001100);
-const uint8_t arptiming[4] = { 0x0C, 0x30, 0xFB, 0x0C };
-
-const int bassbeat[8] = { 0, 0, 1, 0, 0, 1, 0, 1 };
-const int bassline[] = {
-	7, 7, 9, 6, 7, 7, 10, 6, 7, 7, 9, 4, 5, 5, 2, 4,
-	5, 5, 6, 6, 7, 7, 3, 3, 5, 5, 6, 6
-};
-
-const uint8_t leadtimes[] = {
-	1, 2, 3, 4, 5, 6, 28, 14
-};
-const uint8_t leaddata[] = {
-	0x67, 0x24, 0x20, 0x27, 0x20, 0x28, 0x89, 0x0, 0x28, 0x20, 0x27, 0x20, 0x28, 0x89, 0x0, 0x28,
-	0x20, 0x27, 0x20, 0x28,	0x86, 0x0, 0x44, 0x0, 0x63, 0x24, 0x62, 0xA1, 0xE0, 0xE0, 0xE0, 0xE0,
-	0x20, 0x29, 0x20, 0x2A, 0x8B, 0x0, 0x4E, 0x0, 0x6F, 0x30, 0x71, 0xAF, 0xE0, 0xE0, 0xE0, 0xE0,
-	0x20, 0x29, 0x20, 0x2A, 0x8B, 0x0, 0x4E, 0x0, 0x6F, 0x30, 0x6F, 0xAC, 0xE0, 0xE0, 0xE0, 0xE0,
-	0x65, 0x22, 0x20, 0x65, 0x26, 0x87, 0x0, 0x68, 0x69, 0x2B, 0xAA, 0xC0, 0x67, 0x24, 0x20, 0x67,
-	0x28, 0x89, 0x0, 0x68, 0x69, 0x2B, 0xAA, 0xC0, 0x65, 0x22, 0x20, 0x65, 0x26, 0xA7, 0x28, 0x20,
-	0x69, 0x2B, 0xAA, 0x29, 0x20, 0x68, 0x29, 0xAA, 0x2B, 0x20, 0x69, 0x28, 0x69, 0x67,
-};
-const uint8_t leadseq[] = { 0, 1, 0, 2, 0, 1, 0, 3, 4, 5, 6 };
-#define LEADSIZE 174
-
-struct leadvoice_s {
-	uint8_t ptr, timer;
-	uint16_t osc;
-} leads[3] = {
-	{ LEADSIZE, 0, 0 },
-	{ LEADSIZE, 0, 1601 },
-	{ LEADSIZE, 0, 3571 },
-};
-
-uint8_t boosts;
-
-static unsigned char voice_lead(unsigned long i, int voice_nr)
+void avr_tone_generator(const uint8_t& step, const uint16_t& period, uint16_t& counter, const uint8_t& mask, uint8_t& flags)
 {
-#define leadptr leads[voice_nr].ptr
-#define lead_osc leads[voice_nr].osc
-#define leadtimer leads[voice_nr].timer
-
-	if (leadptr == LEADSIZE)
+	if (counter >= period)
 	{
-		if (i == (0x40000 + 0x400 * voice_nr))
+		// reload counter
+		counter -= period;
+		if (counter >= step)
 		{
-			leadptr = -1;
-			leadtimer = 1;
+			counter = 0;
 		}
+
+		// toggle flip-flop
+		if (period >= step)
+			flags ^= mask;
 		else
-			return 0;
+			flags |= mask;
 	}
+	counter += step;
+}
 
-	if (0 == (i & 0x0FF))
-		boosts &= ~(1 << voice_nr);
-	if (0 == (i & 0x1FF))
-		leadtimer--;
-
-	if (0 == leadtimer)
-		leadptr++;
-
-	uint8_t data = leaddata[(leadseq[leadptr >> 4] << 4) | (leadptr & 0xF)];
-
-	if (0 == leadtimer)
+void avr_envelope_generator(const uint16_t& period, uint16_t& counter, uint8_t& config, uint8_t& step)
+{
+	if (counter >= period)
 	{
-		leadtimer = leadtimes[data >> 5];
-		boosts |= 1 << voice_nr;
+		// reload counter
+		counter = 0;
+
+		// update cycle step
+		step += envelopes[config];
+		if (step > 0x1F)
+		{
+			config ^= 0b00000010;
+			step = envelopes[config | 1];
+		}
+	}
+	counter++;
+}
+
+void avr_noise_generator(const uint8_t& period, uint8_t& counter, uint16_t& shift, uint8_t& flags)
+{
+	uint8_t period_x2 = (period << 1);
+	if (counter >= period_x2)
+	{
+		// reload counter
+		counter = 0;
+
+		// update shifter
+		auto b16 = uint8_t((shift ^ (shift >> 3)) << 7);
+		shift >>= 1;
+		shift |= ((flags & 0b10000000) << 8);
+		flags &= 0b01111111;
+		flags |= b16;
+
+		// toggle flip-flop
+		flags |= 0b00111000;
+		if (shift & 0x0001) flags &= 0b11000111;
+	}
+	counter++;
+}
+
+void avr_sample_generator(const uint8_t& e_sample, const uint8_t& volume, const uint8_t& flags, const uint8_t& mask, uint8_t& sample)
+{
+	sample = e_sample;
+	if ((volume & 0x10) == 0) { sample = amplitude4b[volume]; }
+	if ((flags  & mask) == 0) { sample = 0; }
+}
+
+void avr_update(uint8_t& out_l, uint8_t& out_r)
+{
+	if (state.flags & 0b01000000)
+	{
+		state.flags &= 0b10111111;
+		state.e_config = psg.e_shape << 2;
+		state.e_step = envelopes[state.e_config | 1];
+		state.e_counter = 0;
 	}
 
-	uint8_t melody = data & 0x1F;
-	lead_osc += notes[melody];
-	uint8_t sample = ((lead_osc >> 7) & 0x3F) + ((lead_osc >> 7) & 0x1F);
-	return (0 == melody) ? 0 : ((boosts & (1 << voice_nr)) ? sample : THREEQUARTERS(sample));
+	avr_tone_generator(PSG_C_PER_S, psg.a_period, state.a_counter, 0b00000001, state.flags);
+	avr_tone_generator(PSG_C_PER_S, psg.b_period, state.b_counter, 0b00000010, state.flags);
+	avr_tone_generator(PSG_C_PER_S, psg.c_period, state.c_counter, 0b00000100, state.flags);
+
+	for (int i = 0; i < PSG_C_PER_S; ++i)
+	{
+		avr_noise_generator(psg.n_period, state.n_counter, state.n_shift, state.flags);
+		avr_envelope_generator(psg.e_period, state.e_counter, state.e_config, state.e_step);
+	}
+
+	uint8_t output = (state.flags | psg.mixer);
+	output &= output >> 3;
+
+	uint8_t a_sample, b_sample, c_sample;
+	uint8_t e_sample = amplitude5b[state.e_step];
+	avr_sample_generator(e_sample, psg.a_volume, output, 0b00000001, a_sample);
+	avr_sample_generator(e_sample, psg.b_volume, output, 0b00000010, b_sample);
+	avr_sample_generator(e_sample, psg.c_volume, output, 0b00000100, c_sample);
+	b_sample >>= 1;
+
+	out_l = a_sample + b_sample;
+	out_r = c_sample + b_sample;
 }
 
-static inline unsigned char voice_arp(unsigned long i)
+void avr_write(uint8_t reg, uint8_t data)
 {
-	static uint16_t arp_osc = 0;
-	uint8_t arpptr = arpseq1[arpseq2[i >> 16]][(i >> 14) & 3];
-	if (!(i & (1 << 13)))
-		arpptr >>= 4;
-	arpptr = arpeggio[arpptr & 0xF][(i >> 8) & 1];
-	if (!(i & 0x80))
-		arpptr >>= 4;
-
-	int note = arpnotes[arpptr & 0xF];
-	arp_osc += note;
-
-	if ((i >> 13) <= 15)
-		return 0;
-
-	uint8_t timing = arptiming[(i >> 12) & 3];
-	if (!((timing << ((i >> 9) & 7)) & 0x80))
-		return 0;
-
-	return (arp_osc & (1 << 12)) ? 0 : 35;
+	if (reg < 14)
+	{
+		psg.reg[reg] = data;
+		if (reg == 0x0D)
+		{
+			psg.e_shape = (data & 0x0F);
+			state.flags |= 0b01000000;
+		}
+	}
 }
 
-static inline unsigned char voice_bass(unsigned long i)
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+WAVEFORMATEX fmt;
+std::ifstream stream;
+int skip;
+
+void init()
 {
-	static uint16_t bassosc = 0, flangeosc = 0;
-	uint8_t bassptr = (i >> 13) & 0xF;
-	if (i >> 19)
-		bassptr |= 0x10;
-	int note = notes[bassline[bassptr]];
-	if (bassbeat[(i >> 10) & 7])
-		note <<= 1;
-	bassosc += note;
-	flangeosc += note + 1;
-	unsigned char ret = ((bassosc >> 8) & 0x7F) + ((flangeosc >> 8) & 0x7F);
-	return ((i >> 6) & 0xF) == 0xF ? 0 : ret;
+	stream.open("sample.psg", std::fstream::binary);
+	if (stream) stream.ignore(16); else exit(1);
+	skip = 0;
+
+	avr_init();
 }
 
-static inline uint8_t next_sample()
+bool play()
 {
-	static unsigned long i = 0;//x40000;
-	uint8_t ret = voice_lead(i, 0) + THREEQUARTERS(voice_lead(i, 1) >> 1) + (voice_lead(i, 2) >> 2) + (voice_bass(i) >> 2) + voice_arp(i);
-	i++;
-	if ((i >> 13) == ARPSIZE)
-		i = 16 << 13;
-	return ret;
+	if (skip) skip--;
+	else
+	{
+		// skip frame begin marker 0xFF or
+		// pending argument of 0xFE command
+		if (!stream.ignore(1)) return false;
+
+		// loop through register values
+		// until next frame marker
+		for (uint8_t v1, v2;;)
+		{
+			if (stream.peek() == EOF ) break;
+			if (stream.peek() == 0xFF) break;
+			if (stream.peek() == 0xFE)
+			{
+				// skip 0xFE command byte
+				stream.ignore(1);
+
+				// peek next byte if available
+				if (stream.peek() == EOF) return false;
+				v2 = uint8_t(stream.peek());
+
+				// setup number of frames to skip
+				skip = (v2 * 4 - 1);
+				break;
+			}
+
+			// read register and value
+			stream.get((char&)v1);
+			if (!stream.get((char&)v2)) return false;
+
+			// update frame state
+			avr_write(v1, v2);
+		}
+	}
+	return true;
 }
 
 void fill(uint8_t* data)
 {
-	static uint8_t max = 0;
+	if (!play()) exit(1);
 
-	for (int j = 0; j < 4096; j++)
+	uint8_t out_l, out_r;
+	for (int i = 0; i < BUFFER_SIZE; i += SAMPLE_SIZE)
 	{
-		data[j] = next_sample();
-		if (data[j] > max)
-		{
-			max = data[j];
-			printf("%x\n", max);
-		}
+		avr_update(out_l, out_r);
+
+		data[i] = ((out_l + out_r) / 2);
+		
 	}
 }
 
-static const WAVEFORMATEX fmt = {
-	WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0
-};
-
 int main(int argc, char* argv[])
 {
+	fmt.wFormatTag = WAVE_FORMAT_PCM;
+	fmt.nSamplesPerSec = SAMPLE_RATE;
+	fmt.nAvgBytesPerSec = (SAMPLE_RATE * SAMPLE_SIZE);
+	fmt.wBitsPerSample = (SAMPLE_SIZE * 8);
+	fmt.nChannels = PWM_CHANNELS;
+	fmt.nBlockAlign = SAMPLE_SIZE;
+	fmt.cbSize = 0;
+
 	HWAVEOUT out;
 	HRESULT rc = waveOutOpen(&out, WAVE_MAPPER, &fmt, NULL, NULL, CALLBACK_NULL);
-	if (rc != MMSYSERR_NOERROR)
-	{
-		printf("error %d on open\n");
+	if (rc != MMSYSERR_NOERROR) 
 		exit(1);
-	}
 
-	WAVEHDR bufs[2] = {
-		{ (char*)malloc(4096), 4096 },
-		{ (char*)malloc(4096), 4096 },
+	WAVEHDR bufs[8] = 
+	{
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
+		{ (char*)malloc(BUFFER_SIZE), BUFFER_SIZE },
 	};
 	int i = 0;
+
+	init();
 
 	fill((uint8_t*)bufs[i].lpData);
 	bufs[i].dwFlags = WHDR_PREPARED;
 	waveOutPrepareHeader(out, bufs + i, sizeof(WAVEHDR));
 	waveOutWrite(out, bufs + i, sizeof(WAVEHDR));
-	i ^= 1;
+	i = (i + 1) % 8;
 
 	while (!(GetAsyncKeyState(VK_ESCAPE) & 1))
 	{
@@ -222,9 +374,7 @@ int main(int argc, char* argv[])
 		bufs[i].dwFlags = WHDR_PREPARED;
 		waveOutPrepareHeader(out, bufs + i, sizeof(WAVEHDR));
 		waveOutWrite(out, bufs + i, sizeof(WAVEHDR));
-		i ^= 1;
+		i = (i + 1) % 8;
 		while (waveOutUnprepareHeader(out, bufs + i, sizeof(WAVEHDR)) == WAVERR_STILLPLAYING);
 	}
 }
-
-#endif

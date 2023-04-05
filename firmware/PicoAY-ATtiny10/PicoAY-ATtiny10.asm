c49612fa24d2aec3f4d8b3164cddf7e46da6da4a
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2023-04-05 19:42:11 +0300

    Now each project has private source code witch uses common blocks of code

--- firmware/PicoAY-ATtiny10/PicoAY-ATtiny10.asm
+++ firmware/PicoAY-ATtiny10/PicoAY-ATtiny10.asm
@@ -13,51 +13,90 @@
 
     .equ    F_CPU       = 8000000
     .equ    F_PSG       = 1750000
+    .equ    MAX_AMP     = 170
 
     .include "../PicoAY-Blocks.asm"
 
-; ------------------------------------------------------------------------------
-; MCU SPECIFIC MACRO
-; ------------------------------------------------------------------------------
+; ==============================================================================
+; FLASH
+; ==============================================================================
 
-    .equ    UART_INT_ADDR = INT0addr
-    .equ    TIMER_OVF_REG = TIFR0
-    .equ    TIMER_OVF_FLG = TOV0
-    .equ    PWM_CHANNEL_A = OCR0AL
-    .equ    PWM_CHANNEL_B = OCR0BL
+.cseg
+    .org    0x0000
+    rjmp    main
+    .org    INT0addr
+    rjmp    sw_uart_rx_isr
 
-.macro setup_cpu_clock
-    ; Setup main CPU clock to 8 Mhz without prescaler --------------------------
+; ==============================================================================
+; DATA
+; ==============================================================================
+
+    data_reg_mask()
+    data_amp_4bit(MAX_AMP)
+    data_amp_5bit(MAX_AMP)
+    data_envelopes(32)
+
+; ==============================================================================
+; CODE
+; ==============================================================================
+
+main:
+    code_setup_sram()
+    code_setup_data_access()
+
+    ; Setup main CPU clock to 8 Mhz without prescaler
     ldi     AL, 0xD8                ; Write correct signature to Configuration
     out     CCP, AL                 ; Change Protection register and set clock
     out     CLKPSR, ZERO            ; division factor to 1 for 8 MHz
-.endm
 
-.macro setup_uart_rx_interrupt
-    ; Setup external interrupt INT0 on PB2 for UART RX -------------------------
+    ; Setup external interrupt INT0 on PB2 for UART RX
     cbi     DDRB,  PORTB2           ; Set PORTB2 as input
     sbi     PUEB,  PUEB2            ; Enable pull-up resistor on PORTB2
     cbi     EICRA, ISC00            ; Falling edge of INT0 generates an
     sbi     EICRA, ISC01            ; interrupt request
     sbi     EIMSK, INT0             ; Allow INT0 ISR execution
-.endm
 
-.macro setup_8bit_pwm_timer
-    ; Setup Timer0 for Fast PWM 8-bit with 0xFF top ----------------------------
+    ; Setup Timer0 for Fast PWM 8-bit with 0xFF top
     sbi     DDRB, PORTB0            ; Set PORTB0 and PORTB1 as output
     sbi     DDRB, PORTB1            ; for Fast PWM (OC0A and OC0B)
     ldi     AL, M(WGM00) | M(COM0A1) | M(COM0B1)
     out     TCCR0A, AL              ; Clear OC0A/OC0B on compare match
     ldi     AL, M(WGM02) | M(CS00)        
     out     TCCR0B, AL              ; Fast PWM with no prescaling
-.endm
 
-.macro setup_unused_hardware
-    ; TODO
-.endm
+    ; Setup everything else and start emulation
+    code_setup_and_start_emulator()
 
-; ------------------------------------------------------------------------------
-; IMPLEMENTATION
-; ------------------------------------------------------------------------------
+    ; Software UART implementation
+    code_sw_uart_rx_isr()
 
-    .include "../PicoAY-Impl.asm"
+loop:
+    ; Waiting for timer overflow and samples output
+    code_sync_and_out(TIFR0, TOV0, OCR0AL, OCR0BL)
+
+    ; Update tone, noise and envelope generators
+    ldi     AL, FDIV                ; 1
+    code_update_tone(a_period, a_counter, chA) ; max:24 min:12
+    code_update_tone(b_period, b_counter, chB) ; max:24 min:12
+    code_update_tone(c_period, c_counter, chC) ; max:24 min:12
+    code_reset_envelope()           ; max:12  min:3
+    code_update_noise_envelope()    ; max:324 min:100
+    code_apply_mixer()              ; max:6   min:6
+
+    ; Compute channels samples and stereo/mono output
+    ldi     ZL, low(P(amp_5bit))    ; 1   Get envelope amplitude from table
+    ldp     AL, e_stp               ; 3   using envelope step as index
+    ldi     BL, low(P(amp_4bit))    ; 1
+    code_compute_sample(a_volume, chA, XL) ; max:9 min:7
+    code_compute_sample(b_volume, chB, BH) ; max:9 min:7
+    code_compute_sample(c_volume, chC, XH) ; max:9 min:7
+    code_compute_output(STEREO_ABC) ; max:3 min:3
+    rjmp    loop                    ; 2   Go to main loop next iteration
+
+; ==============================================================================
+; SRAM
+; ==============================================================================
+
+.dseg
+    .org    SRAM_START
+    sram_psg_regs_and_state()
--- firmware/PicoAY-ATtiny25/PicoAY-ATtiny25.asm
+++ firmware/PicoAY-ATtiny25/PicoAY-ATtiny25.asm
@@ -8,47 +8,58 @@
 ;                                  +--------+                                  ;
 ;                                                                              ;
 
-; ------------------------------------------------------------------------------
+; ==============================================================================
 ; DEFINES
-; ------------------------------------------------------------------------------
+; ==============================================================================
 
     .equ    F_CPU       = 16000000
     .equ    F_PSG       = 1750000
+    .equ    MAX_AMP     = 170
 
     .include "../PicoAY-Blocks.asm"
 
-; ------------------------------------------------------------------------------
-; MCU SPECIFIC MACRO
-; ------------------------------------------------------------------------------
+; ==============================================================================
+; FLASH
+; ==============================================================================
 
-    .equ    UART_INT_ADDR = INT0addr
-    .equ    TIMER_OVF_REG = TIFR
-    .equ    TIMER_OVF_FLG = TOV0
-    .equ    PWM_CHANNEL_A = OCR1A
-    .equ    PWM_CHANNEL_B = OCR1B
+.cseg
+    .org    0x0000
+    rjmp    main
+    .org    INT0addr
+    rjmp    sw_uart_rx_isr
 
-.macro setup_cpu_clock
-    ; Do nothing
-.endm
+; ==============================================================================
+; DATA
+; ==============================================================================
 
-.macro setup_uart_rx_interrupt
-    ; Setup external interrupt INT0 on PB2 for UART RX -------------------------
+    data_reg_mask()
+    data_amp_4bit(MAX_AMP)
+    data_amp_5bit(MAX_AMP)
+    data_envelopes(32)
+
+; ==============================================================================
+; CODE
+; ==============================================================================
+
+main:
+    code_setup_sram()
+    code_setup_data_access()
+
+    ; Setup external interrupt INT0 on PB2 for UART RX
     cbi     DDRB,  PORTB2           ; Set PORTB2 as input
     sbi     PORTB, PORTB2           ; Enable pull-up resistor on PORTB2
     ldi     AL, M(ISC01)            ; Falling edge of INT0 generates an
     out     MCUCR, AL               ; interrupt request
     ldi     AL, M(INT0)             ; Allow INT0 ISR execution
     out     GIMSK, AL               ;
-.endm
 
-.macro setup_8bit_pwm_timer
-    ; Setup Timer0 for Phase Correct PWM 8-bit with 0xFF top -------------------
+    ; Setup Timer0 for Phase Correct PWM 8-bit with 0xFF top
     ldi     AL, M(WGM00)            ; This setup generates timer overflow event
     out     TCCR0A, AL              ; at frequency of 31250 Hz, that is used for 
     ldi     AL, M(CS00)             ; sample generation
     out     TCCR0B, AL              ;
 
-    ; Setup Time1 for High Speed PWM 8-bit with 0xFF top -----------------------
+    ; Setup Time1 for High Speed PWM 8-bit with 0xFF top
     sbi     DDRB, PORTB1            ; Set PORTB1 and PORTB4 as output
     sbi     DDRB, PORTB4            ; for PWM (OC1A and OC1B)
 ;   ldi     AL, M(PLLE) | M(PCKE)
@@ -61,14 +72,40 @@
     out     OCR1C, AL
     ldi     AL, 0x7F
     out     OCR1A, AL
-.endm
 
-.macro setup_unused_hardware
-    ; TODO
-.endm
+    ; Setup everything else and start emulation
+    code_setup_and_start_emulator()
 
-; ------------------------------------------------------------------------------
-; IMPLEMENTATION
-; ------------------------------------------------------------------------------
+    ; Software UART implementation
+    code_sw_uart_rx_isr()
 
-    .include "../PicoAY-Impl.asm"
+loop:
+    ; Waiting for timer overflow and samples output
+    code_sync_and_out(TIFR, TOV0, OCR1A, OCR1B)
+
+    ; Update tone, noise and envelope generators
+    ldi     AL, FDIV                ; 1
+    code_update_tone(a_period, a_counter, chA) ; max:24 min:12
+    code_update_tone(b_period, b_counter, chB) ; max:24 min:12
+    code_update_tone(c_period, c_counter, chC) ; max:24 min:12
+    code_reset_envelope()           ; max:12  min:3
+    code_update_noise_envelope()    ; max:324 min:100
+    code_apply_mixer()              ; max:6   min:6
+
+    ; Compute channels samples and stereo/mono output
+    ldi     ZL, low(P(amp_5bit))    ; 1   Get envelope amplitude from table
+    ldp     AL, e_stp               ; 3   using envelope step as index
+    ldi     BL, low(P(amp_4bit))    ; 1
+    code_compute_sample(a_volume, chA, XL) ; max:9 min:7
+    code_compute_sample(b_volume, chB, BH) ; max:9 min:7
+    code_compute_sample(c_volume, chC, XH) ; max:9 min:7
+    code_compute_output(STEREO_ABC) ; max:3 min:3
+    rjmp    loop                    ; 2   Go to main loop next iteration
+
+; ==============================================================================
+; SRAM
+; ==============================================================================
+
+.dseg
+    .org    SRAM_START
+    sram_psg_regs_and_state()
